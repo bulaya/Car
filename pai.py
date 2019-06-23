91cbc75b1fb0ded97a2602afc33c8653f2837132
刘治远 <714775038@qq.com>
2019-06-24 00:47:48 +0800

    添加界面

--- pai.py
+++ pai.py
@@ -1,5 +1,68 @@
-import cv2
 from hyperlpr import *
+import sys
+import importlib
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+
+
+def selectPath():
+    global path_
+    path_ = filedialog.askopenfilename()
+    path.set(path_)
+
+
+def readpic():
+    image = cv2.imread(path_)
+    detect(image)
+    print(HyperLPR_PlateRecogntion(image)[0][0])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def run():
+    importlib.reload(sys)
+
+    global path
+    window = tk.Tk()
+    window.title('车牌识别')
+
+    fm1 = tk.Frame(window)
+    fm2 = tk.Frame(window)
+    fm3 = tk.Frame(window)
+
+    path = tk.StringVar()
+
+    Ltop = tk.Label(fm1, text="请选择图片路径")
+    B1 = tk.Button(fm2, text="路径选择", command=selectPath)
+    E1 = tk.Entry(fm2, textvariable=path, bd=5)
+    B2 = tk.Button(fm2, text="确定", command=readpic)
+    Lbot1 = tk.Label(fm3, text="学号：xxx 姓名：xxx")
+    Lbot2 = tk.Label(fm3, text="学号：xxx 姓名：xxx")
+
+    Ltop.pack(side=tk.TOP)
+    B1.pack(side=tk.LEFT)
+    E1.pack(side=tk.LEFT)
+    B2.pack(side=tk.LEFT)
+    Lbot1.pack(side=tk.BOTTOM)
+    Lbot2.pack(side=tk.BOTTOM)
+
+    fm1.pack(side=tk.TOP)
+    fm2.pack(side=tk.TOP)
+    fm3.pack(side=tk.TOP)
+
+    sw = window.winfo_screenwidth()
+    # 得到屏幕宽度
+    sh = window.winfo_screenheight()
+    # 得到屏幕高度
+    ww = 300
+    wh = 100
+    # 窗口宽高为100
+    x = (sw - ww) / 2
+    y = (sh - wh) / 3
+    window.geometry("%dx%d+%d+%d" % (ww, wh, x, y))
+
+    window.mainloop()
 
 
 def detect(image):
@@ -26,8 +89,4 @@ def detect(image):
 
 
 if __name__ == '__main__':
-    image = cv2.imread('1.jpg')
-    detect(image)
-    print(HyperLPR_PlateRecogntion(image)[0][0])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    run()
